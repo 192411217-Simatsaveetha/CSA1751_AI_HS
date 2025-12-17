1fae03b2362d9280d3d22e25959d9b231e74129c
192411217-Simatsaveetha <192411217.simats@saveetha.com>
2025-12-17 15:35:19 +0530

    Add files via upload

---
+++ Add, Append, Extend & Delete.py
@@ -0,0 +1,17 @@
+my_list = []
+
+# Use append instead of add
+my_list.append(1)
+print("List after appending 1:", my_list)
+
+# Append another item
+my_list.append(2)
+print("List after appending 2:", my_list)
+
+# Extend with multiple items
+my_list.extend([3, 4, 5])
+print("List after extending with [3,4,5]:", my_list)
+
+# Remove an item
+my_list.remove(3)
+print("List after removing 3:", my_list)
